879594e8cc4afdd27fd345fb16ea30641eeba95a
Thayrine Santos <42386587+thayrinerenata@users.noreply.github.com>
2024-02-06 00:26:39 -0300

    Sprint 6

--- entrega6/src/Main.java
+++ entrega6/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args)
-    {System.out.println("Seja bem vindo(a) ao COMEX");}
+    public static void main(String[] args){
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Seja bem vindo(a) ao COMEX");
+        System.out.println("------------------------------------------");
+
+        String titulo = ">>> Listagem dos Clientes";
+        String cliente1 = "Policarpo";
+        int idade1 = 2019;
+
+        String cliente2 = "Laila";
+        int idade2 = 2008;
+
+        System.out.println(titulo);
+        System.out.println("------------------------------------------");
+        System.out.println("Nome: " + cliente1);
+        System.out.println("Idade: " + idade1);
+        System.out.println("------------------------------------------");
+        Systwm.out.println("Nome: " + cliente2);
+        System.out.println("Idade: " + idade2);
+
+    }
+
+
 }
 
